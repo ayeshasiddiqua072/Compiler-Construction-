b439af776a408a05bb576a7dae9ae23f25e96802
ayeshasiddiqua072 <113371800+ayeshasiddiqua072@users.noreply.github.com>
2023-10-25 11:45:30 +0100

    Add files via upload

---
+++ CC MID-TERM (072)/CC_Mid_Question2/CC_Mid_Question2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CC_Mid_Question2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(161, 40);
+            label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(118, 26);
+            label1.TabIndex = 2;
+            label1.Text = "LL1 Parser";
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.BackColor = System.Drawing.Color.White;
+            this.ParseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ParseButton.Location = new System.Drawing.Point(145, 218);
+            this.ParseButton.Margin = new System.Windows.Forms.Padding(5);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(186, 47);
+            this.ParseButton.TabIndex = 0;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = false;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTextBox.Location = new System.Drawing.Point(86, 103);
+            this.InputTextBox.Margin = new System.Windows.Forms.Padding(5);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(301, 59);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(86, 333);
+            this.ResultLabel.Margin = new System.Windows.Forms.Padding(5);
+            this.ResultLabel.Multiline = true;
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(308, 58);
+            this.ResultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(502, 447);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.ParseButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ParseButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox ResultLabel;
+    }
+}
+
---
+++ CC MID-TERM (072)/CC_Mid_Question2/CC_Mid_Question2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CC_Mid_Question2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            try
+            {
+                if (ParseS(input) && input.All(char.IsWhiteSpace))
+                {
+                    ResultLabel.Text = "Accepted";
+                }
+                else
+                {
+                    ResultLabel.Text = "Rejected";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultLabel.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private bool ParseS(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("id"))
+            {
+                return ParseE(input.Substring(2));
+            }
+            return false;
+        }
+
+        private bool ParseE(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("+"))
+            {
+                return ParseT(input.Substring(1)) && ParseEPrime(input.Substring(1));
+            }
+            return true; // E can derive ε
+        }
+
+        private bool ParseEPrime(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("id"))
+            {
+                return ParseT(input.Substring(2)) && ParseEPrime(input.Substring(2));
+            }
+            return true; // E' can derive ε
+        }
+
+        private bool ParseT(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("*"))
+            {
+                return ParseF(input.Substring(1)) && ParseTPrime(input.Substring(1));
+            }
+            return true; // T can derive ε
+        }
+
+        private bool ParseTPrime(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("id"))
+            {
+                return ParseF(input.Substring(2)) && ParseTPrime(input.Substring(2));
+            }
+            return true; // T' can derive ε
+        }
+
+        private bool ParseF(string input)
+        {
+            // Skip leading spaces
+            input = input.TrimStart();
+            if (input.StartsWith("id"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
